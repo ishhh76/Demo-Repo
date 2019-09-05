9c42e34a152c469da1944684586f77596aea60a1
ishhh76 <31880357+ishhh76@users.noreply.github.com>
2019-09-05 12:42:26 +0200

    Add files via upload
    Third commit

--- index.js
+++ index.js
@@ -18,38 +18,215 @@ function Remove(){
 
 }
 function ButtonC1(){
-    var x= document.getElementById("detail1")
+    var z= document.getElementById("col1");
+    var x= document.getElementById("detail1");
+    var y= document.getElementById("price1");
     if(x.style.display==="block"){
         x.style.display="none";
-    }
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="570px";
+        }
+}
     else{
-        x.style.display="block";
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="570px";
+        }else{
+            z.style.height="370px";
+        }
     }
+
 }
 function ButtonC2(){
-    var x= document.getElementById("detail2")
+    var z= document.getElementById("col2");
+    var x= document.getElementById("detail2");
+    var y= document.getElementById("price2");
     if(x.style.display==="block"){
         x.style.display="none";
-    }
+        if (y.style.display==="block"){
+            z.style.height="370px";
+        }else{
+            z.style.height="300px";
+        }
+}
     else{
-        x.style.display="block";
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="370px";
+        }
     }
 }
 function priceBtn1(){
-    var x= document.getElementById("price1")
-    if(x.style.display==="block"){
+  //  var z= document.getElementById("col1")
+    var x= document.getElementById("price1");
+    x.style.display="block";
+    document.getElementById("Rem1").style.display="block";
+  /*  if(x.style.display==="block"){
         x.style.display="none";
-    }
+        if (y.style.display==="block"){
+            z.style.height="370px";
+        }else{
+            z.style.height="300px";
+        }
+}
     else{
-        x.style.display="block";
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="370px";
+        }
     }
+    */
 }
 function priceBtn2(){
-    var x= document.getElementById("price2")
+    //  var z= document.getElementById("col1")
+        document.getElementById("price2").style.display="block";
+        document.getElementById("Rem2").style.display="block";
+}
+
+function priceBtn3(){
+    //  var z= document.getElementById("col1")
+        document.getElementById("price3").style.display="block";
+        document.getElementById("Rem3").style.display="block";
+}
+function priceBtn4(){
+    //  var z= document.getElementById("col1")
+        document.getElementById("price4").style.display="block";
+        document.getElementById("Rem4").style.display="block";
+}
+function priceBtn1R(){
+    document.getElementById("price1").style.display="none"
+    document.getElementById("Rem1").style.display="none";
+
+}
+function priceBtn2R(){
+    document.getElementById("price2").style.display="none"
+    document.getElementById("Rem2").style.display="none";
+
+}
+function priceBtn3R(){
+    document.getElementById("price3").style.display="none"
+    document.getElementById("Rem3").style.display="none";
+
+}
+function priceBtn4R(){
+    document.getElementById("price4").style.display="none"
+    document.getElementById("Rem4").style.display="none";
+
+}
+/*
+function priceBtn2(){
+    var z= document.getElementById("col2")
+    var y= document.getElementById("detail2");
+    var x= document.getElementById("price2");
     if(x.style.display==="block"){
         x.style.display="none";
-    }
+        if (y.style.display==="block"){
+            z.style.height="370px";
+        }else{
+            z.style.height="300px";
+        }
+}
     else{
-        x.style.display="block";
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="370px";
+        }
     }
 }
+*/
+function ButtonC3(){
+    var z= document.getElementById("col3");
+    var x= document.getElementById("detail3");
+    var y= document.getElementById("price3");
+    if(x.style.display==="block"){
+        x.style.display="none";
+        if (y.style.display==="block"){
+            z.style.height="370px";
+        }else{
+            z.style.height="300px";
+        }
+}
+    else{
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="370px";
+        }
+    }
+}
+/*
+function priceBtn3(){
+    var z= document.getElementById("col3")
+    var y= document.getElementById("detail3");
+    var x= document.getElementById("price3");
+    if(x.style.display==="block"){
+        x.style.display="none";
+        if (y.style.display==="block"){
+            z.style.height="370px";
+        }else{
+            z.style.height="300px";
+        }
+}
+    else{
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="370px";
+        }
+    }
+}
+*/
+function ButtonC4(){
+    var z= document.getElementById("col4");
+    var x= document.getElementById("detail4");
+    var y= document.getElementById("price4");
+    if(x.style.display==="block"){
+        x.style.display="none";
+        if (y.style.display==="block"){
+            z.style.height="370px";
+        }else{
+            z.style.height="300px";
+        }
+}
+    else{
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="370px";
+        }
+    }
+}
+/*
+function priceBtn4(){
+    var z= document.getElementById("col4")
+    var y= document.getElementById("detail4");
+    var x= document.getElementById("price4");
+    if(x.style.display==="block"){
+        x.style.display="none";
+        if (y.style.display==="block"){
+            z.style.height="370px";
+        }else{
+            z.style.height="300px";
+        }
+}
+    else{
+        x.style.display="block"
+        if (y.style.display==="block"){
+            z.style.height="470px";
+        }else{
+            z.style.height="370px";
+        }
+    }
+}
+*/
